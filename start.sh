8ef419f1155447f72a01376ec2b39665e5b28705
ayhsuic <ayhsuic@outlook.com>
2025-11-29 15:39:37 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -e
 
 # 生成 teldrive 配置文件
-echo "正在生成 Teldrive 配置文件: /config/config.toml"
+echo "正在生成 Teldrive 配置文件: /.teldrive/config.toml"
 echo "[db]
 data-source = \"${DB_DATA_SOURCE}\"
 prepare-stmt = ${DB_PREPARE_STMT}
@@ -15,15 +15,15 @@ allowed-users = [\"${JWT_ALLOWED_USERS}\"]
 secret = \"${JWT_SECRET}\"
 
 [tg.uploads]
-encryption-key = \"${TG_UPLOADS_ENCRYPTION_KEY}\"" > "/config/config.toml"
+encryption-key = \"${TG_UPLOADS_ENCRYPTION_KEY}\"" > "/.teldrive/config.toml"
 
 # 生成 Rclone 配置文件
-echo "正在生成 Rclone 配置文件: /config/rclone.conf"
+echo "正在生成 Rclone 配置文件: /.config/rclone/rclone.conf"
 echo "[teldrive]
 type = teldrive
 api_host = http://localhost:8080
-access_token = ${TELDRIVE_ACCESS_TOKEN}" > "/config/rclone.conf"
+access_token = ${TELDRIVE_ACCESS_TOKEN}" > "/.config/rclone/rclone.conf"
 
 # 启动 teldrive
 echo "Starting teldrive..."
-/teldrive run --config /config/config.toml
+/teldrive run --config /.teldrive/config.toml
