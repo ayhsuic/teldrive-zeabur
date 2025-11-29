16c004159bed2c11c4f9bea9d8591617325a8476
ayhsuic <ayhsuic@outlook.com>
2025-11-29 11:59:05 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 set -e
 
+# 生成 teldrive 配置文件
+echo "正在生成 Teldrive 配置文件: /config/config.toml"
+echo "[db]
+data-source = \"${DB_DATA_SOURCE}\"
+prepare-stmt = ${DB_PREPARE_STMT}
+
+[db.pool]
+enable = ${DB_POOL_ENABLE}
+
+[jwt]
+allowed-users = [\"${JWT_ALLOWED_USERS}\"]
+secret = \"${JWT_SECRET}\"
+
+[tg.uploads]
+encryption-key = \"${TG_UPLOADS_ENCRYPTION_KEY}\"" > "/config/config.toml"
+
 # 启动 teldrive
 echo "Starting teldrive..."
 /teldrive run --config /config/config.toml &
